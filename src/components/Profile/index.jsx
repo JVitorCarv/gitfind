8047b1e4650136ef9ba3df3ba70f7f898df617a7
Vitor Carvalho <vitor.velozo2003@gmail.com>
2023-07-07 12:59:16 -0300

    Fix some responsibility issues

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -5,7 +5,7 @@ const Profile = ({user}) => {
     return (
         <>
             <div className='perfil'>
-                <ProfilePicture user={user}/>
+                <ProfilePicture className="profile-pic" user={user}/>
                 <div className='profile-text'>
                     <a target='_blank' rel="noreferrer" href={`https://github.com/${user.login}`}>
                         <h3>{user.name}</h3>
